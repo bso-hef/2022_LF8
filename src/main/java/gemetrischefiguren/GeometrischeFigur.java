e22c605087b81b4e6ff0efe163d003c4a0bc7465
s.schmitt <fdai2003@gmail.com>
2023-02-21 10:07:20 +0100

    Erste Version

---
+++ src/main/java/gemetrischefiguren/GeometrischeFigur.java
@@ -0,0 +1,22 @@
+package gemetrischefiguren;
+
+public abstract class GeometrischeFigur {
+
+    protected String farbe;
+
+    protected int zoomfaktor;
+
+
+    public void setzteFarbe(String farbe) {
+
+    }
+
+    public void zoomeAuf(int zoomfaktor) {
+
+    }
+
+    public void anzeigen() {
+
+    }
+
+}
---
+++ src/main/java/gemetrischefiguren/Kreis.java
@@ -0,0 +1,5 @@
+package gemetrischefiguren;
+
+public class Kreis extends GeometrischeFigur {
+
+}
